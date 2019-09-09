0b73622b80a0987b50ebf46570fe446433af8b38
marcvifi10 <36198307+marcvifi10@users.noreply.github.com>
2019-09-09 11:05:22 +0200

    09

---
+++ 58 - Aplicación completa II/AplicacionCompleta/resources/views/admin/users/edit.blade.php
@@ -0,0 +1,8 @@
+<html>
+	<head>
+		<meta charset="UTF8">
+	</head>
+	<body>
+		<h1>PÁGINA PARA EDITAR USUARIOS</h1>
+	</body>
+</html>
---
+++ 58 - Aplicación completa II/AplicacionCompleta/resources/views/admin/users/index.blade.php
@@ -0,0 +1,8 @@
+<html>
+	<head>
+		<meta charset="UTF8">
+	</head>
+	<body>
+		<h1>PÁGINA PRINCIPAL DEL ADMINISTRADOR</h1>
+	</body>
+</html>
