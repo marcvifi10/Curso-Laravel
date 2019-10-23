d64b0e78884918bf2b307cb014d6ca2889760aba
marcvifi10 <marcvifi10@outlook.es>
2019-10-23 16:24:40 +0200

    laravel

--- 59 - Aplicación completa III/AplicacionCompleta/resources/views/admin/users/create.blade.php
+++
@@ -1,8 +0,0 @@
-<html>
-	<head>
-		<meta charset="UTF8">
-	</head>
-	<body>
-		<h1>PÁGINA PARA AGREGAR USUARIOS</h1>
-	</body>
-</html>
--- 61 - Aplicación completa V. Formulario creación de usuario/AplicacionCompleta/resources/views/admin/users/destroy.blade.php
+++
@@ -1,8 +0,0 @@
-<html>
-	<head>
-		<meta charset="UTF8">
-	</head>
-	<body>
-		<h1>PÁGINA PARA ELIMINAR USUARIOS</h1>
-	</body>
-</html>
